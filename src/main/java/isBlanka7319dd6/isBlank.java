3c544fe2ddcff25d781d99e986194950f26644bd
lucas-salves <salves.lucas@outlook.com>
2021-12-02 16:06:46 -0300

    Add a7319dd6 training: isBlank

---
+++ src/main/java/isBlanka7319dd6/isBlank.java
@@ -0,0 +1,15 @@
+package isBlanka7319dd6;
+
+public class isBlank {
+
+    public static void main(String[] args) {
+        System.out.println( "ABC".isBlank() );
+        
+        System.out.println( " ABC".isBlank() );
+        
+        System.out.println( "  ".isBlank() );
+        
+        System.out.println( "".isBlank() );
+    }
+    
+}
