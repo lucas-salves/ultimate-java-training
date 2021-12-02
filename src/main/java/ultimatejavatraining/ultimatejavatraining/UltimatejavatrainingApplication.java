f3cf711e1c9fdb60a4ec50b2065428ff58503173
lucas-salves <salves.lucas@outlook.com>
2021-12-02 10:40:17 -0300

    project creation

---
+++ src/main/java/ultimatejavatraining/ultimatejavatraining/UltimatejavatrainingApplication.java
@@ -0,0 +1,13 @@
+package ultimatejavatraining.ultimatejavatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimatejavatrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimatejavatrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ultimatejavatraining/ultimatejavatraining/UltimatejavatrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ultimatejavatraining.ultimatejavatraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimatejavatrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
