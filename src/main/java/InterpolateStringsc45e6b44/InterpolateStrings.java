3d5bab0f6c2489ff952909617bcac6d8b396e767
Lucas Sousa Alves <salves.lucas@outlook.com>
2022-02-08 15:45:09 -0300

    Add c45e6b44 training: Interpolate Strings

---
+++ src/main/java/InterpolateStringsc45e6b44/InterpolateStrings.java
@@ -0,0 +1,11 @@
+package InterpolateStringsc45e6b44;
+
+public class InterpolateStrings {
+    public static void main(String[] args) {
+        String nome = "Fulano";
+        
+        int idade = 23;
+        
+        System.out.printf("%s tem %d anos \n",nome, idade);
+    }
+}
