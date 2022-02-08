6352d0e676ac425973724d06900ba068b8891af0
Lucas Sousa Alves <salves.lucas@outlook.com>
2022-02-08 15:34:20 -0300

    Add d8849856 training: Convert a uint32 timestamp to java util date

---
+++ src/main/java/TimestampToJavaUtilDated8849856/TimestampToJavaUtilDate.java
@@ -0,0 +1,15 @@
+package TimestampToJavaUtilDated8849856;
+
+import java.util.Date;
+
+public class TimestampToJavaUtilDate {
+
+    public static void main(String[] args) {
+
+        var timestamp = 41651321;
+        
+        var date = new Date(timestamp);
+        
+        System.out.println(date);
+    }
+}
